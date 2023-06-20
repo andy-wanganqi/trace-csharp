1f34ea1f9dedb34b8c1f98ed48d29c1d31526663
andy-wanganqi <andy.wanganqi@gmail.com>
2023-06-20 16:43:58 +1200

    CSharp 9 Review

---
+++ CSharp9/CovariantReturns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9
+{
+    public class ClassA
+    {
+        public virtual ClassA Foo(ClassA input) { return input; }
+    }
+
+    public class ClassB : ClassA
+    {
+        public override ClassB Foo(ClassA input)
+        {
+            return new();
+        }
+    }
+}
---
+++ CSharp9/StaticClassWarning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9
+{
+    internal class StaticClassWarning
+    {
+        static class StaticClass
+        {
+            public static void Thing() { }
+        }
+
+        void M(object o)
+        {
+            // warning: cannot use a static type in 'is' or 'as'
+            if (o is StaticClass)
+            {
+                Console.WriteLine("Can't happen");
+            }
+            else
+            {
+                Console.WriteLine("o is not an instance of a static class");
+            }
+        }
+    }
+}
